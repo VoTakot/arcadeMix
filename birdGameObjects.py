ca6849642bf257d0a49b0e081cfb775018fafb28
krak251 <botkinulsa@gmail.com>
2026-01-12 18:49:40 +0300

    Основные Классы игры

---
+++ birdGameObjects.py
@@ -0,0 +1,28 @@
+import arcade
+
+
+class Bird(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def update(self, delta_time):
+        pass
+
+
+class Colon(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def update(self, delta_time):
+        pass
+
+
+class Finish(arcade.Sprite):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def update(self, delta_time):
+        pass
