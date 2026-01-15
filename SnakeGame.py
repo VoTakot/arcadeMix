6c5694670d2e2c6fa41bb0d5e37f3baaefbddf26
BLDolph <danacecurov@gmail.com>
2026-01-15 18:00:47 +0300

    start

---
+++ SnakeGame.py
@@ -0,0 +1,25 @@
+import arcade
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 600
+SCREEN_TITLE = 'Snake'
+
+
+class Snake(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height, SCREEN_TITLE)
+        self.width = width
+        self.height = height
+        self.game_over = False
+
+    def update(self):
+        pass
+
+    def on_draw(self):
+        pass
+
+
+def setup_game(width=1000, height=600):
+    game = Snake(width, height)
+    game.setup()
+    return game
