74fbc49784b505d9685727d072b12633fac2abf2
BLDolph <danacecurov@gmail.com>
2026-01-15 18:21:59 +0300

    start

---
+++ SnakeObjects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="snake" tilewidth="96" tileheight="96" tilecount="12" columns="4">
+ <image source="../../Загрузки/kenney_platformer-art-deluxe/auxebiyr4shpnur92aedj3uzua0.png" width="384" height="288"/>
+</tileset>
