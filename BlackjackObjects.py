58d1da1e219465910c5dc6f065d8204093ba7a18
VoTakot <matveisergeeiv2012@gmail.com>
2026-01-22 15:42:08 +0300

    Прототип карты

--- BlackjackObjects.py
+++ BlackjackObjects.py
@@ -1,5 +1,24 @@
 import arcade
+import sqlite3
+
+connection = sqlite3.connect('cards.db')
+
+cursor = connection.cursor()
 
 
-class Card(arcade.View):
-    pass
+class Card(arcade.Sprite):
+    def __init__(self, card):
+        super().__init__()
+
+        card_data = cursor.execute("""
+        select texture, points
+        from CARDS
+        where id = ?
+        """, (card,)).fetchall()
+        print(card_data[0])
+
+        self.texture = arcade.load_texture(str(card_data[0][0]))
+        if int(card_data[0][1]) != 11:
+            self.points = int(card_data[0][1])
+        else:
+            self.points = (1, 11)
